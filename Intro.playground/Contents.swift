031a2eeb1006fe3ecc8b8bd1fc0de58cb92e5797
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2017-09-27 08:09:15 -0600

    Adding a methods playground to make sure I learn about Swift.

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -1,5 +1,31 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//Explicit Type declaration with no value
+var ramen :String
+//Implicit Type declaration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge + 54322456
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Cody"
+print(myName)
+//myName = soup
+//Can't assign into a let once assigned
 
-var str = "Hello, playground"
+let sample = myAge * Int(someValue)
+print(Int (3.0000000000))
+let derp = "7"
+
+
